fb34cd12225c3987ccea86b7e174cfaa36076b33
Norman Lim <frenoid@gmail.com>
2022-07-22 15:21:21 +0800

    feat: update function to send message to a queue

--- HttpExample/index.js
+++ HttpExample/index.js
@@ -1,41 +1,19 @@
 module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-    try {
-        context.log('JavaScript HTTP trigger function processed a request.');
-
-        // Read incoming data
-        const name = (req.query.name || (req.body && req.body.name));
-        const sport = (req.query.sport || (req.body && req.body.sport));
-
-        // fail if incoming data is required
-        if (!name || !sport) {
-
-            context.res = {
-                status: 400
-            };
-            return;
-        }
-
-        // Add or change code here
-        const message = `${name} likes ${sport}`;
-
-        // Construct response
-        const responseJSON = {
-            "name": name,
-            "sport": sport,
-            "message": message,
-            "greetings": "hello - this is new",
-            "success": true
-        }
-
+    if (req.query.name || (req.body && req.body.name)) {
+        // Add a message to the Storage queue,
+        // which is the name passed to the function.
+        context.bindings.msg = (req.query.name || req.body.name);
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: responseJSON,
-            contentType: 'application/json'
-        };
-    } catch(err) {
-        context.res = {
-            status: 500
+            body: "Hello " + (req.query.name || req.body.name)
         };
     }
-}
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+};
