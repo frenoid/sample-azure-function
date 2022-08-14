3baeeebdfb2d0c09f83ebc8e06deb1cc57cacba7
Norman Lim <frenoid@gmail.com>
2022-08-14 14:34:50 +0800

    feat: print datetime on return

--- HttpExample/index.js
+++ HttpExample/index.js
@@ -2,12 +2,18 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
+        // Get date and time
+        var today = new Date();
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date+' '+time;
+
         // Add a message to the Storage queue,
         // which is the name passed to the function.
         context.bindings.msg = (req.query.name || req.body.name);
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: "Hello " + (req.query.name || req.body.name) + ". You queried at " + dateTime
         };
     }
     else {
